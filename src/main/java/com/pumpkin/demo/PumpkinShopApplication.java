820a90762ac15a0375e676ab8d84ac242498450b
william2271 <william22711@gmail.com>
2025-05-22 12:12:09 -0400

    Start fron the project

---
+++ src/main/java/com/pumpkin/demo/PumpkinShopApplication.java
@@ -0,0 +1,13 @@
+package com.pumpkin.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PumpkinShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PumpkinShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pumpkin/demo/PumpkinShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pumpkin.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PumpkinShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
